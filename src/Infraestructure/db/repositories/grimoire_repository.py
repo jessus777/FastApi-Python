8e167500196f7cb4ddf2217b3435fa2108320bd9
Jesus Jimenez Rendon <jesusJimenezRE777@outlook.com>
2024-05-28 23:58:58 -0600

    feat: add methods in repository to request and grimoire

--- src/Infraestructure/db/repositories/grimoire_repository.py
+++ src/Infraestructure/db/repositories/grimoire_repository.py
@@ -18,3 +18,58 @@ class GrimoiresRepository:
             except Exception as exception:
                 session.rollback()
                 raise Exception
+            
+            
+    @classmethod
+    def get_grimoires_by_id(cls, id: str) -> any:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                grimoire = (
+                    database.session
+                    .query(GrimoireEntity)
+                    .filter_by(id = id)
+                    .first()
+                    
+                )
+                return grimoire
+            except Exception as exception:
+                session.rollback()
+                raise Exception(f"Error fetching grimoire by ID: {str(exception)}")  
+            
+            
+    @classmethod
+    def get_grimoires_list(cls) -> any:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                grimoires = (
+                    database.session
+                    .query(GrimoireEntity)
+                    .all()
+                    
+                )
+                return grimoires
+            except Exception as exception:
+                session.rollback()
+                raise Exception(f"Error fetching grimoire all: {str(exception)}") 
+            
+            
+    @classmethod
+    def delete_grimoire_by_id(cls, id: str) -> None:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                grimoire = (
+                    session.query(GrimoireEntity)
+                    .filter_by(id=id)
+                    .first()
+                )
+                if grimoire:
+                    session.delete(grimoire)
+                    session.commit()
+                else:
+                    raise Exception(f"Grimoire with ID {id} not found")
+            except Exception as exception:
+                session.rollback()
+                raise Exception(f"Error deleting grimoire: {str(exception)}")
--- src/Infraestructure/db/repositories/request_repository.py
+++ src/Infraestructure/db/repositories/request_repository.py
@@ -1,6 +1,7 @@
 from src.Infraestructure.db.settings.connection import  DBConnectionHandler 
 from src.Infraestructure.db.entities.requests import Request as RequestEntity
 from uuid import uuid4
+from sqlalchemy.exc import SQLAlchemyError
 
 class RequestsRepository:
     @classmethod
@@ -15,7 +16,63 @@ class RequestsRepository:
                 )
                 session.add(new_registry)
                 session.commit()
+            except SQLAlchemyError as exception:
+                session.rollback()
+                print(f"Error occurred while inserting request: {exception}")
+                raise Exception
+            
+    
+    @classmethod
+    def get_request_by_id(cls, id: str) -> any:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                grimoire = (
+                    database.session
+                    .query(RequestEntity)
+                    .filter_by(id = id)
+                    .first()
+                    
+                )
+                return grimoire
+            except SQLAlchemyError as exception:
+                session.rollback()
+                raise Exception(f"Error fetching request by ID: {str(exception)}") 
+            
+    @classmethod
+    def patch_request_status(cls, id: str, status: str) -> None:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                request = (
+                    session.query(RequestEntity)
+                    .filter_by(id=id)
+                    .first()
+                )
+                if request:
+                    request.status = status
+                    session.commit()
+                else:
+                    raise Exception(f"Request with ID {id} not found")
+            except SQLAlchemyError as exception:
+                session.rollback()
+                raise Exception(f"Error updating request status: {str(exception)}")
+    
+    @classmethod
+    def delete_request_by_id(cls, id: str) -> None:
+        with DBConnectionHandler() as database:
+            session = database.get_session()
+            try:
+                request = (
+                    session.query(RequestEntity)
+                    .filter_by(id=id)
+                    .first()
+                )
+                if request:
+                    session.delete(request)
+                    session.commit()
+                else:
+                    raise Exception(f"Grimoire with ID {id} not found")
             except Exception as exception:
                 session.rollback()
-                print(f"Error occurred while inserting student: {exception}")
-                raise Exception
+                raise Exception(f"Error deleting grimoire: {str(exception)}")
